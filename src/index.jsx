576c2c2f64e0a98a791c359f2d27d90ba78583aa
realaisha <realaisha1@gmail.com>
2025-08-28 15:29:06 +0100

    running npm build

--- src/index.jsx
+++ src/index.jsx
@@ -27,7 +27,8 @@ export default function LandingPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://pph-app.onrender.com/predict", {
+
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
